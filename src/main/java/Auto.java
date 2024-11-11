ab90ce51caafa3e5f9e3dc09f0c75c1830e2577e
Mateo Perez Gutierrez <mperezgu@unal.edu.co>
2024-11-11 00:53:36 -0500

    Intento 1
    Primer intento de solucionar la actividad

---
+++ src/main/java/Auto.java
@@ -0,0 +1,35 @@
+package test;
+
+public class Auto {
+    String modelo;
+    int precio;
+    Asiento[] asientos;
+    String marca;
+    Motor motor;
+    int registro;
+    static int cantidadCreados = 0;
+
+    public int cantidadAsientos() {
+        int contador = 0;
+        for (Asiento asiento : asientos) {
+            if (asiento != null) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public String verificarIntegridad() {
+        if (motor.registro != this.registro) {
+            return "Las piezas no son originales";
+        }
+        for (Asiento asiento : asientos) {
+            if (asiento != null && asiento.registro != this.registro) {
+                return "Las piezas no son originales";
+            }
+        }
+
+        return "Auto original";
+    }
+}
+
